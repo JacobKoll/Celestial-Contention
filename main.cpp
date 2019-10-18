7bf245d037c7301af5055fb79fffc9722654aa3d
JacobKoll <jacob.m.koll@gmail.com>
2019-10-17 20:10:55 -0500

    -Various tests to get things working added cmake as the build system.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "HelloWorld.h"
+#include <iostream>
 
 int main()
 {
+	HelloWorld date(2019, 10, 16);
+    std::cout<<"Year: " << date.getYear() << "\n";
+    std::cout<<"Month: " << date.getMonth() << "\n";
+    std::cout<<"Day: " << date.getDay() << "\n";
     return 0;
 }
